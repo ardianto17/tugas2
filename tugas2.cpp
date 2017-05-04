c156a46b66f0d6aea36f9e0883905744620ae4b4
ardianto17 <ayobangkit2@gmail.com>
2017-05-04 16:49:11 +0700

    Add files via upload

---
+++ tugas2.cpp
@@ -0,0 +1,58 @@
+#mohammad ardianto #14201010
+ #include <stdio.h>
+
+#include <windows.h>
+
+int main(VOID) { STARTUPINFO si;
+
+PROCESSJNFORMATION pi;
+
+/* allocate memory */
+
+ZeroMemory(&si, sizeof(si));
+
+si.cb = sizeof(si);
+
+ZeroMemory(&pi, sizeof(pi));
+
+/* create child process */
+
+if (CreateProcess(NTJLL, /* use command line */
+
+“C: \WINDOWS\system32\mspaint .exe”, /* command */
+
+NULL, /* don’t inherit process handle */
+
+NULL, /* don’t inherit thread handle */
+
+FALSE, /* disable handle inheritance */
+
+O, /* no creation flags */
+
+NULL, /* use parent’s environment block */
+
+NULL, /* use parent’s existing directory */
+
+Scsi,
+
+&pi))
+
+{
+
+fprintf(stderr, “Create Process Failed”);
+
+return —1;
+
+}
+
+/* parent will wait for the child to complete */
+
+WaitForSingleObject(pi.hProcess, INFINITE);
+
+printf (“Child Complete”);
+
+/* close handles */
+
+CloseHandle(pi .hProcess);
+
+CloseHandle (pi . hThread); }
